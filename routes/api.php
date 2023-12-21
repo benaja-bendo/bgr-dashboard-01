ec6d2823be2d14471a7d124db2443aac279cf734
benaja-bendo <benaja.bendo02@gmail.com>
2023-12-21 21:45:41 +0100

    amorçage de la config api qui va permettre de se connecter aux informations d'un locataire particulier.

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,12 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get(
+    '/item',
+    function (Request $request) {
+        $tenant = \App\Models\Tenant::find('foo');
+        tenancy()->initialize($tenant);
+        $users = \App\Models\User::all();
+        return response()->json($users);
+    });
