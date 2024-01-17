0f576f45fcadd662855e9ed5ae1d6528942c2c42
benaja-bendo <benaja.bendo02@gmail.com>
2024-01-17 23:33:25 +0100

    [🔄️refactor] rename class

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class UsersTableSeeder extends Seeder
+class UserSeeder extends Seeder
 {
     /**
      * Run the database seeds.
