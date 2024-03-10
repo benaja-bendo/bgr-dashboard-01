fc128218d1a06009ba99ec6d0556d07882466a8e
benji_mac <benaja.bendo02@gmail.com>
2024-03-10 14:01:22 +0100

    feat: add tenant and minio to filesystems.php

--- config/filesystems.php
+++ config/filesystems.php
@@ -56,6 +56,25 @@ return [
             'throw' => false,
         ],
 
+        'tenant' => [
+            'driver' => 'local',
+            'root' => storage_path('app/public/tenants'),
+            'url' => env('APP_URL').'/storage/tenants',
+            'visibility' => 'public',
+            'throw' => false,
+        ],
+
+        'minio' => [
+            'driver' => 's3',
+            'key' => env('MINIO_ACCESS_KEY'),
+            'secret' => env('MINIO_SECRET_KEY'),
+            'region' => env('MINIO_DEFAULT_REGION'),
+            'bucket' => env('MINIO_BUCKET'),
+            'endpoint' => env('MINIO_ENDPOINT', 'http://127.0.0.1:8900'),
+            'use_path_style_endpoint' => env('MINIO_USE_PATH_STYLE_ENDPOINT', false),
+            'throw' => false,
+        ],
+
     ],
 
     /*
