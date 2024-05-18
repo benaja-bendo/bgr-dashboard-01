9f80cca3289bf052442df5aac3a037086c4b77a1
benji_mac <benaja.bendo02@gmail.com>
2024-05-18 14:47:02 +0200

    config scan app for générate file

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,10 +11,46 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 use Spatie\Permission\Traits\HasRoles;
+use OpenApi\Attributes as OA;
 
+#[
+    OA\Schema(
+        title: "User",
+        description: "User model",
+        required: ["last_name", "first_name", "email", "password"],
+        properties: [
+            new OA\Property(property: "last_name", description: "Last name of the user", type: "string"),
+            new OA\Property(property: "first_name", description: "First name of the user", type: "string"),
+            new OA\Property(property: "middle_names", description: "Middle names of the user", type: "string"),
+            new OA\Property(property: "avatar", description: "Avatar of the user", type: "string"),
+            new OA\Property(property: "gender", description: "Gender of the user", type: "string"),
+            new OA\Property(property: "email", description: "Email of the user", type: "string"),
+            new OA\Property(property: "password", description: "Password of the user", type: "string"),
+            new OA\Property(property: "birth_date", description: "Birth date of the user", type: "string"),
+        ],
+        type: "object",
+        example: [
+            "last_name" => "Doe",
+            "first_name" => "John",
+            "middle_names" => "Smith",
+            "avatar" => "https://example.com/avatar.jpg",
+            "gender" => "Male",
+            "email" => "exampel@mail.com",
+            "password" => "password",
+            "birth_date" => "1990-01-01",
+            "addresses" => [],
+            "number_phones" => [],
+            "email_verified_at" => "2021-01-01",
+            "created_at" => "2021-01-01",
+            "updated_at" => "2021-01-01",
+            "role" => []
+        ],
+
+    )
+]
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable,HasRoles,SoftDeletes;
+    use HasApiTokens, HasFactory, Notifiable, HasRoles, SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
